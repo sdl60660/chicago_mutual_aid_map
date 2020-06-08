840b41363b837a1de43da962bedcc87c089d556f
sdl60660 <learnersd@gmail.com>
2020-06-08 13:04:27 -0400

    Finished processing hours to determine open/close status and added corresponding coloring (still need legend). Also connected listing with markers so that a click on a listing triggers a marker popup.

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,36 @@
 
 // Initialize global variables
 var phoneBrowsing = false;
-var addressLink
+
+var organzations;
+var markers;
+
+var addressLink;
+var orderedWeekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var markerColoringDict = {
+    'Open for Dropoff/Pickup': 'rgb(50,205,50)',
+    'Open only for Dropoff': "rgb(44,123,182)",
+    'Open only for Pickup': "rgb(171,217,233)",
+    'Currently Closed': "rgb(215,25,28)",
+    'Status Unknown': "rgb(110,110,110)"
+}
+
+var listingColoringDict = {
+    'Open for Dropoff/Pickup': 'rgba(50,205,50,0.4)',
+    'Open only for Dropoff': "rgba(44,123,182,0.4)",
+    'Open only for Pickup': "rgba(171,217,233,0.4)",
+    'Currently Closed': "rgba(215,25,28,0.4)",
+    'Status Unknown': "rgba(110,110,110,0.4)"
+}
+
 
 
 // Determine if the user is browsing on mobile and adjust worldMapWidth if they are
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     phoneBrowsing = true;
 }
+
 
 function processFieldGroup(d, fieldNames) {
     var returnText = '';
@@ -27,9 +50,21 @@ function processFieldGroup(d, fieldNames) {
     return returnText;
 }
 
+function timeStringtoNum(timeString) {
+    var hourVal = parseInt(timeString.slice(0, timeString.indexOf(':')));
+    var minuteVal = parseInt(timeString.slice(timeString.length - 8, timeString.length - 6))
+
+    var fractionalHour = hourVal + (minuteVal / 60)
+
+    if (timeString.slice(timeString.length - 2) == 'PM') {
+        fractionalHour += 12;
+    }
+
+    return fractionalHour;
+}
+
 
 function formatHours(weekdayString, startTime, endTime) {
-    var orderedWeekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var weekdayAbbreviatons = {
         'Sunday': 'Sun',
         'Monday': 'Mon',
@@ -127,6 +162,137 @@ function generatePopUpText(d) {
 
 }
 
+function determineOpenStatus(d, siteFunction) {
+    var currentDateTime = new Date();
+    var currentWeekday = orderedWeekdays[currentDateTime.getDay()];
+    var dayWindow = false;
+    var timeWindow = false;
+
+    // If missing the needed fields to detemrine open status, just return unknown
+    if ((!d['Location End Date'] && !d[(siteFunction + ' Weekdays')]) || (!d[(siteFunction + ' Start')] || !d[(siteFunction + ' End')])) {
+        return 'Unknown';
+    }
+
+    // If this is a popup/temporary location, to be open we must be within the start/end dates
+    if (d['Location Start Date'] && d['Location End Date']) {
+        if (new Date(d['Location Start Date']) <= currentDateTime && new Date(d['Location End Date']) >= currentDateTime) {
+            dayWindow = true;
+        }
+    }
+    // Otherwise, the current weekday just needs to be part of the listed open weekdays
+    else {
+        if (d[(siteFunction + ' Weekdays')].includes(currentWeekday)) {
+            dayWindow = true;
+        }
+    }
+
+    // Determine whether the current time is within the open hour window
+    var numericalStartTime = timeStringtoNum(d[(siteFunction + ' Start')]);
+    var numericalEndTime = timeStringtoNum(d[(siteFunction + ' End')]);
+    var currentTime = currentDateTime.getHours() + (currentDateTime.getMinutes() / 60)
+
+    if (numericalStartTime <= currentTime && numericalEndTime >= currentTime) {
+        timeWindow = true;
+    }
+
+    if (timeWindow && dayWindow) {
+        return 'Open'
+    }
+    else {
+        return 'Closed'
+    }
+
+}
+
+function updateOrgs() {
+    var displayData = organzations;
+
+    Object.keys(markers._layers).forEach( function(layer) {
+        markers.removeLayer(layer);
+    });
+    $(".site-listing")
+        .remove();
+
+    if ($('#search-val').val().length > 0) {
+        // Get search query from box
+        var searchTerm = $('#search-val').val().toLowerCase();
+
+        // Filter to only groups whose names/neighborhoods contain the search term
+        displayData = organzations.filter(function(d) {
+            return ( d.Organization.toLowerCase().indexOf(searchTerm) !== -1 ||
+                d.Neighborhood.toLowerCase().indexOf(searchTerm) !== -1)               
+        });
+    }
+
+    displayData.sort(function(a,b) {
+        if (a.Organization < b.Organization) {
+            return -1;
+        }
+        if (a.Organization > b.Organization) {
+            return 1;
+        }
+        return 0;
+    })
+
+    displayData.forEach(function(d, i) {
+        // Determine Open Status
+        var dropoffStatus = determineOpenStatus(d, 'Dropoff');
+        var pickupStatus = determineOpenStatus(d, 'Pickup');
+
+        var consolidatedStatus;
+        if (dropoffStatus == 'Open' && pickupStatus == 'Open') {
+            consolidatedStatus = 'Open for Dropoff/Pickup';
+        }
+        else if (dropoffStatus == 'Open') {
+            consolidatedStatus = 'Open only for Dropoff';
+        }
+        else if (pickupStatus == 'Open') {
+            consolidatedStatus = 'Open only for Pickup';
+        }
+        else if (dropoffStatus == 'Closed' || pickupStatus == 'Closed') {
+            consolidatedStatus = 'Currently Closed';
+        }
+        else {
+            consolidatedStatus = 'Status Unknown';
+        }
+
+        // Create and save a reference to each marker
+        var marker = L.circleMarker([d.Latitude, d.Longitude], {'color': markerColoringDict[consolidatedStatus], 'radius': 5, 'weight': 1, 'fillOpacity': 0.7, myCustomId: i}).addTo(markers);
+        markers.addLayer(marker);
+
+        addressLink = "https://maps.google.com?saddr=Current+Location&daddr=" + encodeURI(d.Address);
+
+        var popupText = '<div class="marker-text">';
+        popupText += generatePopUpText(d);
+        popupText += '</div>';
+        marker.bindPopup(popupText);
+
+        var listingText = '<div class="site-listing" style="background-color:' + listingColoringDict[consolidatedStatus] + ';" markerId="' + i + '"">'
+        // background-color: rgba(220,220,220, 0.4);
+
+        listingText += generatePopUpText(d);
+        listingText += '</div>'
+
+        $( "#location-list" ).append(listingText);
+    })
+
+    $(".site-listing")
+        .on("click tap", function() {
+            var markerId = $(this).attr('markerId');
+            var matchingMarker = Object.values(markers._layers).find(function(d) {
+                return d.options.myCustomId == markerId;
+            });
+            matchingMarker.fire('click');
+
+    })
+
+}
+
+$("#search-val")
+    .on("keyup", function() {
+        updateOrgs();
+    })
+
 
 var promises = [
     d3.json("static/data/chicago.geojson"),
@@ -135,8 +301,7 @@ var promises = [
 
 Promise.all(promises).then(function(allData) {
     var neighborhoodGeoJSON = allData[0];
-    var organzations = allData[1];
-    console.log(allData[1]);
+    organzations = allData[1];
 
     var mapCenter = d3.geoCentroid(allData[0]);
 
@@ -154,67 +319,15 @@ Promise.all(promises).then(function(allData) {
 
     L.geoJSON(neighborhoodGeoJSON, {weight: 1}).addTo(mymap);
 
-    organzations.sort(function(a,b) {
-        if (a.Organization < b.Organization) {
-            return -1;
-        }
-        if (a.Organization > b.Organization) {
-            return 1;
-        }
-        return 0;
-    })
 
+    // setup a marker group
+    markers = L.layerGroup().addTo(mymap);
 
-    markers = {};
-    organzations.forEach(function(d, i) {
-        // var marker = L.marker([d.Latitude, d.Longitude]).addTo(mymap);
-        // var marker = L.circleMarker([d.Latitude, d.Longitude], {'color': 'green', 'radius': 5, 'weight': 1, 'fillOpacity': 0.7, myCustomId: ('marker-' + i)}).addTo(mymap);
-
-        // Create and save a reference to each marker
-        var marker = L.circleMarker([d.Latitude, d.Longitude], {'color': 'green', 'radius': 5, 'weight': 1, 'fillOpacity': 0.7, myCustomId: ('marker-' + i)}).addTo(mymap);
-
-
-        // Add the ID
-        // console.log(markers[i])
-        // markers[i]._icon.id = i;
-
-        addressLink = "https://maps.google.com?saddr=Current+Location&daddr=" + encodeURI(d.Address);
-
-        var popupText = '<div class="marker-text">';
-        popupText += generatePopUpText(d);
-        popupText += '</div>';
-        marker.bindPopup(popupText);
-
-        var listingText = '<div class="site-listing">'
-        listingText += generatePopUpText(d);
-        listingText += '</div>'
-
-        $( "#location-list" ).append(listingText);
-    })
-    
-
+    // Initialize markers/listings
+    updateOrgs();
 
 
 });
 
-
-
-// function generateListingText(d) {
-//     var listingText = '<div class="site-listing">'
-//     listingText += "<div style='text-align:center;'><h5>" + d.Organization + "</h5></div>";
-//     listingText += "<span style='color:black'><strong>Neighborhood</strong>: " + d.Neighborhood + "</span></br>";
-//     listingText += "<span style='color:black'><strong>Address</strong>: <a href='" + addressLink + "' target='_blank'>" + d.Address + "</a></span></br></br>";
-
-//     listingText += "<span style='color:black'><strong>Looking For</strong>: " + d["Looking for"] + "</span></br></br>";
-
-
-//     listingText += "<span style='color:black'><strong>Email</strong>: " + d.Email + "</span></br>";
-//     listingText += "<span style='color:black'><strong><a href='" + d["Donation Link"] + ">'>Donation Link</a></strong>" + "</span></br>";
-
-
-//     listingText += "</div>";
-
-//     return listingText;
-// }
 
 
